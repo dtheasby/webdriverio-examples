c36b178fd51f0efd4531612196ed756a17ffe112
dtheasby <dtheasby@hotmail.co.uk>
2016-02-25 13:32:40 +0000

    Added files via upload

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var gulp = require('gulp');
+var selenium = require('selenium-standalone');
+var webdriver = require('gulp-webdriver');
+
+
+gulp.task('selenium', function (done) {
+    selenium.start(function (err, child) {
+      if (err) return done(err);
+      selenium.child = child;
+      done();
+    });
+});
+
+gulp.task('runTest', ['selenium'], function() {
+  return gulp.src(path.join(__dirname, 'wdio.conf.js'))
+    .pipe(webdriver({logLevel: 'verbose'})).once('end', function() {
+      selenium.child.kill();
+    });
+});
+
