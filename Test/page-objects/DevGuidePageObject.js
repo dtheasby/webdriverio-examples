a8c458f3b6dddf7f7011285c49780518433490ef
dtheasby <dtheasby@hotmail.co.uk>
2016-04-06 14:27:45 +0100

    Added files via upload

---
+++ Test/page-objects/DevGuidePageObject.js
@@ -0,0 +1,22 @@
+var DevGuide = (function() {
+    
+  function DevGuide() {
+    this.testRunnerButton = '[data-open="testrunner"]';
+    this.testRunnerDropdown = ".commands.testrunner";
+    
+};
+
+DevGuide.prototype.getElementId = function(ele) {
+    return ele.value.ELEMENT;   
+};
+
+DevGuide.prototype.numberOfSubElements = function(ID) {
+   return browser.elementIdElements(ID,'<a>').then(function(elementsArray){
+        return Object.keys(elementsArray.value).length;
+    });
+};
+
+return DevGuide;
+})();
+
+module.exports = DevGuide;
---
+++ Test/page-objects/HomePageObject.js
@@ -0,0 +1,13 @@
+var HomePage = (function() {
+
+    function HomePage() {
+        this.url = "http://webdriver.io/";
+        this.guideButton = "[href='/guide.html']";
+        this.apiButton = "[href='/api.html']";
+    };
+
+    return HomePage;
+
+})();
+
+module.exports = HomePage;
