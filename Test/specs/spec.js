329e015d2a9275b62de06a0aa8bfbd388fa14640
dtheasby <dtheasby@hotmail.co.uk>
2016-04-06 14:28:10 +0100

    Added files via upload

---
+++ Test/specs/spec.js
@@ -0,0 +1,38 @@
+var HomePageObject = require("../page-objects/HomePageObject.js")
+var DevGuidePageObject = require("../page-objects/DevGuidePageObject.js")
+
+
+describe("First Spec", function() {
+    
+    var home;
+    var devGuide; 
+    
+    before(function() {
+       home = new HomePageObject(); 
+       devGuide = new DevGuidePageObject();
+    });
+    
+    it("should navigate to the webdriverIO homepage", function(){
+        return browser.url(home.url)
+        .click(home.guideButton)
+        .getUrl().then(function(url){
+            console.log(url) // outputs "http://webdriver.io/guide.html"
+            expect(url).to.equal(home.url + "guide.html");
+        });      
+    });    
+    
+    it("should count the number of testrunner menu subelements", function() {
+        return browser.url(home.url)
+            .click(home.guideButton)
+            .click(devGuide.testRunnerButton)
+            .element(devGuide.testRunnerDropdown)
+            .then(function(elem) {
+                return devGuide.getElementId(elem);
+            }).then(function(id) {
+                return devGuide.numberOfSubElements(id)
+            }).then(function(numberOfElements) {
+                console.log(numberOfElements)
+                return expect(numberOfElements).to.equal(5);
+            });
+    });
+});
